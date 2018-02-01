18a1859542ff41f7fdee9e19429fc3800d4e004f
Unknown <evgentrigub@yandex.ru>
2018-02-01 13:57:26 +0300

    TopBanks отображается индекс по времени

--- Otzovik.py
+++ Otzovik.py
@@ -2,50 +2,50 @@ import requests
 import bs4 as bs
 from openpyxl import Workbook
 
-
-def get_otziv_link():
-    otziv_links = []
-    count = 0
-    response = requests.get('http://otzovik.com/reviews/sberbank_rossii/order_date_desc/')
-    soup = bs.BeautifulSoup(response.text, 'lxml')
-    otzivi_group = soup.find_all('div', class_='catproduct2')
-    for otziv_link in otzivi_group:
-        for link in otziv_link.find_all('a'):
-            otziv_links.append(link['href'])
-            count = count + 1
-    n = 2
-    while n < 4:
-        URL = 'http://otzovik.com/reviews/sberbank_rossii/order_date_desc/'
-        response = requests.get(URL + str(n) + '/')
-        print(URL + str(n) + '/')
+class Otzovik:
+    def get_otziv_link():
+        otziv_links = []
+        count = 0
+        response = requests.get('http://otzovik.com/reviews/sberbank_rossii/order_date_desc/')
         soup = bs.BeautifulSoup(response.text, 'lxml')
         otzivi_group = soup.find_all('div', class_='catproduct2')
         for otziv_link in otzivi_group:
             for link in otziv_link.find_all('a'):
                 otziv_links.append(link['href'])
                 count = count + 1
-        n = n + 1
-    print(count)
+        n = 2
+        while n < 4:
+            URL = 'http://otzovik.com/reviews/sberbank_rossii/order_date_desc/'
+            response = requests.get(URL + str(n) + '/')
+            print(URL + str(n) + '/')
+            soup = bs.BeautifulSoup(response.text, 'lxml')
+            otzivi_group = soup.find_all('div', class_='catproduct2')
+            for otziv_link in otzivi_group:
+                for link in otziv_link.find_all('a'):
+                    otziv_links.append(link['href'])
+                    count = count + 1
+            n = n + 1
+        print(count)
 
-    return otziv_links
+        return otziv_links
 
 
-links = get_otziv_link()
-print(links)
+    links = get_otziv_link()
+    print(links)
 
-def get_otziv_info(otziv_links):
-    otziv_textS= []
-    for el in otziv_links:
-        response = requests.get(el)
-        soup = bs.BeautifulSoup(response.text, 'lxml')
-        otziv_text = soup.find_all('div', {'itemprop':'description'})
-        otziv_text.append(otziv_textS)
-    return otziv_textS
-print(get_otziv_info(links))
+    def get_otziv_info(otziv_links):
+        otziv_textS= []
+        for el in otziv_links:
+            response = requests.get(el)
+            soup = bs.BeautifulSoup(response.text, 'lxml')
+            otziv_text = soup.find_all('div', {'itemprop':'description'})
+            otziv_text.append(otziv_textS)
+        return otziv_textS
+    print(get_otziv_info(links))
 
-link = 'https://otzovik.com/review_5932159.html'
-response = requests.get(link)
-soup = bs.BeautifulSoup(response.text, 'lxml')
-otziv_text = soup.find_all('div', {'itemprop':'description'})
+    link = 'https://otzovik.com/review_5932159.html'
+    response = requests.get(link)
+    soup = bs.BeautifulSoup(response.text, 'lxml')
+    otziv_text = soup.find_all('div', {'itemprop':'description'})
 
-print(otziv_text)
+    print(otziv_text)
--- TopBanks.py
+++ TopBanks.py
@@ -5,44 +5,17 @@ import numpy as np
 from openpyxl import Workbook
 
 
-def get_otziv_info():
-    elements = []
-    URL = 'http://topbanki.ru/banks/sbrf/'
-    response = requests.get(URL)
-    # otziv_text = soup.find_all('p')
-    soup = bs.BeautifulSoup(response.text, 'lxml')
-    otziv_group = soup.find_all('div', class_='response_table__data')
-    for otziv in otziv_group:
-        group = []
-        text = otziv.find('p').text
-        time = otziv.find('li').text
-        if (otziv.find('div', class_='office') == None):
-            office = "-"
-        else:
-            office1 = otziv.find('div', class_='office')
-            office = office1.find(class_='mr10').text
-
-        if (otziv.find('div', class_='author') == None):
-            cat = "-"
-        else:
-            category = otziv.find('div', class_='author')
-            cat = category.find('span', class_='tag-mini mr10').text
-        group.append(text)
-        group.append(time)
-        group.append(office)
-        group.append(cat)
-        elements.append(group)
-
-    n = 2
-    while n < 3:
+class TopBanks:
+    def get_otziv_info():
+        elements = []
         URL = 'http://topbanki.ru/banks/sbrf/'
-        response = requests.get(URL + 'page' + str(n))
+        response = requests.get(URL)
         soup = bs.BeautifulSoup(response.text, 'lxml')
         otziv_group = soup.find_all('div', class_='response_table__data')
         for otziv in otziv_group:
             group = []
             text = otziv.find('p').text
-            time = otziv.find('li').text
+            #time = otziv.find('li').text
             if (otziv.find('div', class_='office') == None):
                 office = "-"
             else:
@@ -55,16 +28,72 @@ def get_otziv_info():
                 category = otziv.find('div', class_='author')
                 cat = category.find('span', class_='tag-mini mr10').text
             group.append(text)
-            group.append(time)
+            #group.append(time)
             group.append(office)
             group.append(cat)
             elements.append(group)
-        n = n + 1
 
-    return elements
-elements = get_otziv_info()
+        n = 2
+        while n < 3:
+            URL = 'http://topbanki.ru/banks/sbrf/'
+            response = requests.get(URL + 'page' + str(n))
+            soup = bs.BeautifulSoup(response.text, 'lxml')
+            otziv_group = soup.find_all('div', class_='response_table__data')
+            for otziv in otziv_group:
+                group = []
+                text = otziv.find('p').text
+                #time = otziv.find('li').text
+                if (otziv.find('div', class_='office') == None):
+                    office = "-"
+                else:
+                    office1 = otziv.find('div', class_='office')
+                    office = office1.find(class_='mr10').text
 
-array = np.array(elements)
-df = pd.DataFrame(array)
-#df = df.append(elements)
-print(df)
+                if (otziv.find('div', class_='author') == None):
+                    cat = "-"
+                else:
+                    category = otziv.find('div', class_='author')
+                    cat = category.find('span', class_='tag-mini mr10').text
+                group.append(text)
+                #group.append(time)
+                group.append(office)
+                group.append(cat)
+                elements.append(group)
+            n = n + 1
+
+        return elements
+
+    def get_otziv_time():
+        otziv_data = []
+        URL = 'http://topbanki.ru/banks/sbrf/'
+        response = requests.get(URL)
+        soup = bs.BeautifulSoup(response.text, 'lxml')
+        dates = soup.find_all(class_='actions')
+        a = soup.find_all('ul', {'class': 'actions'})
+        for el in a:
+            data = el.text
+            otziv_data.append(data)
+            ##print(el.text)
+
+        n = 2
+        while n < 3:
+            response = requests.get(URL + 'page' + str(n))
+            soup = bs.BeautifulSoup(response.text, 'lxml')
+            dates = soup.find_all(class_='actions')
+            a = soup.find_all('ul', {'class': 'actions'})
+            for el in a:
+                data = el.text
+                otziv_data.append(data)
+
+            n = n + 1
+
+        return otziv_data
+
+    time = get_otziv_time()
+    elements = get_otziv_info()
+
+    #data = {'text': get_otziv_info()}
+
+    df = pd.DataFrame(elements)
+    df.index = get_otziv_time()
+    print(df)
