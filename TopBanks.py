d3599c17a9a26a259d294c86e4d84f8b1a3c5850
Unknown <evgentrigub@yandex.ru>
2018-02-02 20:00:24 +0300

    Читабельная таблица с правильными датами

--- TopBanks.py
+++ TopBanks.py
@@ -1,12 +1,15 @@
+import datetime
 import requests
 import bs4 as bs
 import pandas as pd
 from pandas import ExcelWriter
 import xlsxwriter as xw
+import openpyxl
 import numpy as np
 
 
 class TopBanks:
+
     def get_otziv_info():
         elements = []
         URL = 'http://topbanki.ru/banks/sbrf/'
@@ -16,22 +19,21 @@ class TopBanks:
         for otziv in otziv_group:
             group = []
             text = otziv.find('p').text
-            # time = otziv.find('li').text
-            if (otziv.find('div', class_='office') == None):
-                office = "None"
-            else:
-                office1 = otziv.find('div', class_='office')
-                office = office1.find(class_='mr10').text
-
             if (otziv.find('div', class_='author') == None):
                 cat = "None"
             else:
                 category = otziv.find('div', class_='author')
                 cat = category.find('span', class_='tag-mini mr10').text
+
+            if (otziv.find('div', class_='office') == None):
+                address = "None"
+            else:
+                address = otziv.find('div', class_='office').text
+                address = address[20:]
+
             group.append(text)
-            # group.append(time)
-            group.append(office)
             group.append(cat)
+            group.append(address)
             elements.append(group)
 
         n = 2
@@ -43,29 +45,28 @@ class TopBanks:
             for otziv in otziv_group:
                 group = []
                 text = otziv.find('p').text
-                # time = otziv.find('li').text
-                if (otziv.find('div', class_='office') == None):
-                    office = "None"
-                else:
-                    office1 = otziv.find('div', class_='office')
-                    office = office1.find(class_='mr10').text
-
                 if (otziv.find('div', class_='author') == None):
                     cat = "None"
                 else:
                     category = otziv.find('div', class_='author')
                     cat = category.find('span', class_='tag-mini mr10').text
+
+                if (otziv.find('div', class_='office') == None):
+                    address = "None"
+                else:
+                    address = otziv.find('div', class_='office').text
+                    address = address[20:]
+
                 group.append(text)
-                # group.append(time)
-                group.append(office)
                 group.append(cat)
+                group.append(address)
                 elements.append(group)
             n = n + 1
 
         return elements
 
     def get_otziv_time():
-        otziv_data = []
+        time = []
         URL = 'http://topbanki.ru/banks/sbrf/'
         response = requests.get(URL)
         soup = bs.BeautifulSoup(response.text, 'lxml')
@@ -73,7 +74,7 @@ class TopBanks:
         a = soup.find_all('ul', {'class': 'actions'})
         for el in a:
             data = el.text
-            otziv_data.append(data)
+            time.append(data)
             ##print(el.text)
 
         n = 2
@@ -84,31 +85,59 @@ class TopBanks:
             a = soup.find_all('ul', {'class': 'actions'})
             for el in a:
                 data = el.text
-                otziv_data.append(data)
+                time.append(data)
 
             n = n + 1
+        return time
 
-        return otziv_data
+    def get_right_time(time):
+        now_date = datetime.date.today()
+        now = now_date.strftime("%d %B %Y")
+        delta = datetime.timedelta(days=1)
+        yesterday_date = now_date - delta
+        right_time = []
+        for el in time:
+            el = el.replace('Сегодня в', now)
+            el = el.replace('Вчера в', yesterday_date.strftime("%d %B %Y"))
+            el = el.replace('January', 'января')
+            el = el.replace('February', 'февраля')
+            el = el.replace('March', 'марта')
+            el = el.replace('April', 'апреля')
+            el = el.replace('May', 'мая')
+            el = el.replace('June', 'июня')
+            el = el.replace('July', 'июля')
+            el = el.replace('August', 'августа')
+            el = el.replace('September', 'сентября')
+            el = el.replace('October', 'октября')
+            el = el.replace('November', 'ноября')
+            el = el.replace('December', 'декабря')
+            right_time.append(el)
+        return right_time
 
-    time = get_otziv_time()
+    def export_to_excel(otzivi, right_time):
+        df = pd.DataFrame(otzivi)
+        df.index = right_time
+        df.columns = ['Текст Отзыва', 'Категория', 'Адрес офиса']
+        df.index.names = ['Дата']
+        # print(df)
+        writer = ExcelWriter('TopBanks.xlsx')
+        df.to_excel(writer, 'Feedbacks')
+        writer.save()
+
+        wb = openpyxl.load_workbook('TopBanks.xlsx')
+        ws = wb.get_sheet_by_name('Feedbacks')
+        dims = {}
+        for row in ws.rows:
+            for cell in row:
+                if cell.value:
+                    dims[cell.column] = max((dims.get(cell.column, 0), len(cell.value)))
+        for col, value in dims.items():
+            # value * коэфициент
+            ws.column_dimensions[col].width = value * 1.5
+
+        wb.save("TopBanks.xlsx")
+
     otzivi = get_otziv_info()
-
-    # data = {'text': get_otziv_info()}
-
-    df = pd.DataFrame(otzivi)
-    df.index = get_otziv_time()
-    df.columns = ['Текст Отзыва', 'Место', 'Категория']
-    df.index.names = ['Дата']
-    #bla = list(df)
-
-    #print(df)
-
-    writer = ExcelWriter('TopBanks.xlsx')
-    df.to_excel(writer, 'Отзывы')
-    writer.save()
-
-    workbook = xw.Workbook('TopBanks.xlsx')
-    worksheet = workbook.get_worksheet_by_name('Отзывы')
-
-
-
+    time = get_otziv_time()
+    right_time = get_right_time(time)
+    final_otzivi = export_to_excel(otzivi, right_time)
